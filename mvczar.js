ad5ef40115c87ad345bb324ae1e8fbd4bf7d77e4
Ben <exactly_88mph@hotmail.com>
2014-08-11 18:16:46 +0100

    namespacing

--- mvczar.js
+++ mvczar.js
@@ -1,65 +1,79 @@
-function Emitter(events) {
+MVCzar = (function() {
 
-    this.handlers = {};
+    var exports = {};
 
-    if (events) {
-        // add initial events
-        for (var event in events) {
-            if (events.hasOwnProperty(event)) {
-                this.on(event, events[event]);
-            }
-        }
-    }
+    // ***************************************
+    // Event Emitter Interface
+    // ***************************************
 
-};
+    function Emitter(events) {
 
-Emitter.prototype.on = function(event, handler) {
+        this._handlers = {};
 
-    if (this.handlers[event]) {
-        this.handlers[event].push(handler);
-    } else {
-        this.handlers[event] = [handler];
-    }
-
-    return this;
-
-};
-
-Emitter.prototype.off = function(event, handler) {
-    
-    if (this.handlers[event]) {
-        if (handler) {
-            // remove all instances of handler
-
-            var i = this.handlers[event].length;
-
-            while (i--) {
-                if (this.handlers[event][i] === handler) {
-                    this.handlers[event].splice(i,1);
+        if (events) {
+            // add initial events
+            for (var event in events) {
+                if (events.hasOwnProperty(event)) {
+                    this.on(event, events[event]);
                 }
             }
-
-        } else {
-            // clear out all event handlers for stated event
-            while (this.handlers[event].length) {
-                this.handlers[event].pop();
-            }
-        } 
-    }
-
-    return this;
-
-};
-
-Emitter.prototype.emit = function(event) {
-
-    // call all handlers for stated event
-    if (this.handlers[event]) {
-        for (var i=0, l=this.handlers[event].length; i<l; i++) {
-            this.handlers[event][i].call(this);
         }
-    }
 
-    return this;
+    };
 
-};
+    Emitter.prototype.on = function(event, handler) {
+
+        if (this._handlers[event]) {
+            this._handlers[event].push(handler);
+        } else {
+            this._handlers[event] = [handler];
+        }
+
+        return this;
+
+    };
+
+    Emitter.prototype.off = function(event, handler) {
+        
+        if (this._handlers[event]) {
+            if (handler) {
+                // remove all instances of handler
+
+                var i = this._handlers[event].length;
+
+                while (i--) {
+                    if (this._handlers[event][i] === handler) {
+                        this._handlers[event].splice(i,1);
+                    }
+                }
+
+            } else {
+                // clear out all event handlers for stated event
+                while (this._handlers[event].length) {
+                    this._handlers[event].pop();
+                }
+            } 
+        }
+
+        return this;
+
+    };
+
+    Emitter.prototype.emit = function(event) {
+
+        // call all handlers for stated event
+        if (this._handlers[event]) {
+            for (var i=0, l=this._handlers[event].length; i<l; i++) {
+                this._handlers[event][i].call(this);
+            }
+        }
+
+        return this;
+
+    };
+
+    exports.Emitter = Emitter;
+
+    return exports;
+
+})();
